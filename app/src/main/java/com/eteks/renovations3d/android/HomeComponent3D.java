8a1f14275f569947542bf8b1ef1f156a5b05d88d
phil <philjord@ihug.co.nz>
2017-03-18 15:44:48 +1300

    disable lights taken out of deoptomize

--- app/src/main/java/com/eteks/renovations3d/android/HomeComponent3D.java
+++ app/src/main/java/com/eteks/renovations3d/android/HomeComponent3D.java
@@ -670,7 +670,7 @@ public class HomeComponent3D extends NewtBaseFragment implements com.eteks.sweet
 		JoglesPipeline.MINIMISE_NATIVE_CALLS_TRANSPARENCY = !deoptomize;
 		JoglesPipeline.MINIMISE_NATIVE_CALLS_TEXTURE = !deoptomize;
 
-		SimpleShaderAppearance.setDISABLE_LIGHTS(deoptomize);
+		// PJ mnot needed now the shaders are per vert SimpleShaderAppearance.setDISABLE_LIGHTS(deoptomize);
 
 		SharedPreferences settings = getActivity().getSharedPreferences(PREFS_NAME, 0);
 		SharedPreferences.Editor editor = settings.edit();
