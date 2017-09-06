e2487942eb3a7477a176390a66f008910fbada80
phil <philjord@ihug.co.nz>
2017-09-06 17:29:56 +1200

    admobmanager cleaned up

--- app/src/main/java/com/eteks/renovations3d/AdMobManager.java
+++ app/src/main/java/com/eteks/renovations3d/AdMobManager.java
@@ -6,10 +6,8 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
@@ -27,15 +25,15 @@ import com.mindblowing.renovations3d.R;
 public class AdMobManager
 {
 
-	private static final boolean ENABLE_INTERSTITIALS = true;
+	private static final boolean ENABLE_INTERSTITIALS = false;
 
 
 	private static final boolean SUPPRESS_ADS = false;//for debug and screenshots overrides force_ads
 	private static final boolean FORCE_DEBUG_ADS = true;//for debug build to still have ads <= PREFER this one! Less dangerous
 	private static final String INTERSTITIAL_POINTS = "INTERSTITIAL_POINTS";
 	private static final String INTERSTITIAL_LAST_SHOWN_TIME = "INTERSTITIAL_LAST_SHOWN_TIME";
-	private static final int INTERSTITIAL_POINTS_THRESHOLD = 3;//10;
-	private static final long INTERSTITIAL_TIME_THRESHOLD = 0;//2 * 24 * 60 * 60 * 1000; // 2 days in ms
+	private static final int INTERSTITIAL_POINTS_THRESHOLD = 10;
+	private static final long INTERSTITIAL_TIME_THRESHOLD = 2 * 24 * 60 * 60 * 1000; // 2 days in ms
 	private Renovations3DActivity renovations3DActivity;
 	private AdRequest.Builder builder = new AdRequest.Builder();
 	private AdView mBasicLowerBannerAdView;
@@ -46,12 +44,10 @@ public class AdMobManager
 	{
 		this.renovations3DActivity = renovations3DActivity;
 
-
 		//example logcat Use AdRequest.Builder.addTestDevice("4A1B3B44655FDE15F64CFD90EFD60699") to get test ads on this device.
 		builder.addTestDevice("56ACE73C453B9562B288E8C2075BDA73");//T580
 		builder.addTestDevice("4A1B3B44655FDE15F64CFD90EFD60699");//I9505
 		builder.addTestDevice("F1F03BC6248C8ECF32CBB4DD027F78B9");//T210
-
 
 		mBasicLowerBannerAdView = (AdView) renovations3DActivity.findViewById(R.id.lowerBannerAdView);
 
@@ -79,7 +75,25 @@ public class AdMobManager
 				MobileAds.initialize(renovations3DActivity.getApplicationContext(), "ca-app-pub-7177705441403385~4026888158");
 				mBasicLowerBannerAdView.loadAd(builder.build());
 				if (ENABLE_INTERSTITIALS)
+				{
 					mInterstitialAd.loadAd(builder.build());
+					mInterstitialAd.setAdListener(new AdListener()
+					{
+						@Override
+						public void onAdClosed()
+						{
+							//This is how I would load the next ad, but because of the time threshold removed to reduce overhead
+							// Load the next interstitial.
+							//mInterstitialAd.loadAd(builder.build());
+						}
+
+						@Override
+						public void onAdFailedToLoad(int errorCode)
+						{
+							Renovations3DActivity.logFireBase(FirebaseAnalytics.Event.POST_SCORE, "INTERSTITIAL onAdFailedToLoad " + errorCode, null);
+						}
+					});
+				}
 			}
 		}
 	}
@@ -207,38 +221,31 @@ public class AdMobManager
 					int points = settings.getInt(INTERSTITIAL_POINTS, 0);
 
 					long lastShownTime = settings.getLong(INTERSTITIAL_LAST_SHOWN_TIME, 0);
-					// is now the time to show an interstitial??
+					// is now the time to show an interstitial?
 					if (points >= INTERSTITIAL_POINTS_THRESHOLD && System.currentTimeMillis() - lastShownTime > INTERSTITIAL_TIME_THRESHOLD)
 					{
-						if (ENABLE_INTERSTITIALS && mInterstitialAd.isLoaded())
+						// we reset either way as we still want the interstitial counts on firebase correct
+						// zero points and record exact time
+						SharedPreferences.Editor editor = settings.edit();
+						editor.putInt(INTERSTITIAL_POINTS, 0);
+						editor.putLong(INTERSTITIAL_LAST_SHOWN_TIME, System.currentTimeMillis());
+						editor.apply();
+
+						if (ENABLE_INTERSTITIALS )
 						{
-							Renovations3DActivity.logFireBaseContent("INTERSTITIAL SHOWN", null);
-							// zero points and record exact time
-							SharedPreferences.Editor editor = settings.edit();
-							editor.putInt(INTERSTITIAL_POINTS, 0);
-							editor.putLong(INTERSTITIAL_LAST_SHOWN_TIME, System.currentTimeMillis());
-							editor.apply();
-
-
-							mInterstitialAd.setAdListener(new AdListener() {
-								@Override
-								public void onAdClosed() {
-									//This is how I would load the next ad, but because of the time threshold removed to reduce overhead
-									// Load the next interstitial.
-									//mInterstitialAd.loadAd(builder.build());
-								}
-								@Override
-								public void onAdFailedToLoad(int errorCode) {
-									Renovations3DActivity.logFireBase(FirebaseAnalytics.Event.POST_SCORE, "INTERSTITIAL onAdFailedToLoad " + errorCode, null);
-								}
-							});
-
-							mInterstitialAd.show();
-
+							if (mInterstitialAd.isLoaded())
+							{
+								Renovations3DActivity.logFireBaseContent("INTERSTITIAL SHOWN", null);
+								mInterstitialAd.show();
+							}
+							else
+							{
+								Renovations3DActivity.logFireBase(FirebaseAnalytics.Event.POST_SCORE, "INTERSTITIAL NOT LOADED", null);
+							}
 						}
 						else
 						{
-							Renovations3DActivity.logFireBase(FirebaseAnalytics.Event.POST_SCORE, "INTERSTITIAL NOT LOADED", null);
+							Renovations3DActivity.logFireBaseContent("INTERSTITIAL WOULD HAVE BEEN SHOWN", null);
 						}
 					}
 				}});
