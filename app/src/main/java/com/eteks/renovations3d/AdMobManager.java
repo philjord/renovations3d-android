8453dd167516d944fef06dfb1e61e94b1ebcbcd3
Philip Jordan <p.j.nz@outlook.com>
2025-07-26 16:09:22 +1200

    AdManager was not removing AdView as it would lock from not being on UIThread
    Also Interstitals removed completely

--- app/src/main/java/com/eteks/renovations3d/AdMobManager.java
+++ app/src/main/java/com/eteks/renovations3d/AdMobManager.java
@@ -1,21 +1,13 @@
 package com.eteks.renovations3d;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.location.LocationManager;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.ads.AdError;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.FullScreenContentCallback;
 import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.RequestConfiguration;
-import com.google.android.gms.ads.ResponseInfo;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.gms.ads.interstitial.InterstitialAd;
@@ -30,7 +22,6 @@ import com.mindblowing.renovations3d.BuildConfig;
 import com.mindblowing.renovations3d.R;
 
 import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,16 +34,10 @@ public class AdMobManager
 {
 	private static final boolean SUPPRESS_ADS = false;//for debug and screenshots overrides force_ads
 	public static final boolean FORCE_DEBUG_ADS = false;//for debug build to still have ads <= PREFER this one! Less dangerous
-	private static final String INTERSTITIAL_POINTS = "INTERSTITIAL_POINTS";
-	private static final String INTERSTITIAL_LAST_SHOWN_TIME = "INTERSTITIAL_LAST_SHOWN_TIME";
-	private static final int INTERSTITIAL_POINTS_THRESHOLD = 10;
-	private static final long INTERSTITIAL_TIME_THRESHOLD = 2 * 24 * 60 * 60 * 1000; // 2 days in ms
 	private Renovations3DActivity renovations3DActivity;
 	private AdView mBasicLowerBannerAdView;
-	private InterstitialAd mInterstitialAd;
 
 	private FirebaseRemoteConfig mFirebaseRemoteConfig;
-
 
 	public AdMobManager(final Renovations3DActivity renovations3DActivity)
 	{
@@ -140,89 +125,75 @@ public class AdMobManager
 	/**
 	 * called by billing to indicate it can now service questions
 	 */
-	public void billingServiceConnected()
-	{
-		if (mBasicLowerBannerAdView != null && renovations3DActivity != null)
-		{
+	public void billingServiceConnected() {
+		if (mBasicLowerBannerAdView != null && renovations3DActivity != null) {
 			// set up admob ads if they don't own ad free option and it's not debug
-			if (shouldSuppressAds())
-			{
+			if (shouldSuppressAds()) {
 				removeBasicLowerBannerAdView();
-			}
-			else
-			{
+			} else {
 				mBasicLowerBannerAdView.loadAd(new AdRequest.Builder().build());
 				Renovations3DActivity.logFireBase(FirebaseAnalytics.Event.POST_SCORE, "MobileAds.initialize ", null);
-
-				if (mFirebaseRemoteConfig.getBoolean("renovations3d_interstitial_enabled")) {
-					InterstitialAd.load(renovations3DActivity.getApplicationContext(), "ca-app-pub-7177705441403385/4587558769", new AdRequest.Builder().build(),
-							new InterstitialAdLoadCallback() {
-								@Override
-								public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
-									// The mInterstitialAd reference will be null until an ad is loaded.
-									mInterstitialAd = interstitialAd;
-								}
-
-								@Override
-								public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
-									mInterstitialAd = null;
-									Renovations3DActivity.logFireBase(FirebaseAnalytics.Event.POST_SCORE, "INTERSTITIAL onAdFailedToLoad " + loadAdError, null);
-								}
-							});
-				}
 			}
 		}
 	}
 
-	public void removeBasicLowerBannerAdView()
-	{
-		if (mBasicLowerBannerAdView != null)
-		{
+	public void removeBasicLowerBannerAdView() {
+		if (mBasicLowerBannerAdView != null) {
 			// same thing should be called when purchase complete
-			mBasicLowerBannerAdView.setEnabled(false);
-			mBasicLowerBannerAdView.setVisibility(View.GONE);
+			//https://stackoverflow.com/questions/4549401/correctly-disable-admob-ads
+			//I'm told by the above this ui threaded part isn't needed
+			renovations3DActivity.runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					mBasicLowerBannerAdView.setEnabled(false);
+					mBasicLowerBannerAdView.setVisibility(View.GONE);
+				}
+			});
+
 			ViewGroup parent = (ViewGroup) mBasicLowerBannerAdView.getParent();
-			if (parent != null)
-			{
+			if (parent != null) {
 				parent.removeView(mBasicLowerBannerAdView);
 				parent.invalidate();
 			}
 		}
 	}
 
-	private boolean shouldSuppressAds()
-	{
+	private boolean shouldSuppressAds() {
 		return mBasicLowerBannerAdView == null || renovations3DActivity == null || SUPPRESS_ADS
 				|| renovations3DActivity.getBillingManager().ownsBasicAdFree();
 	}
 
 	//used by the tutorial to not show ads until finished
-	public void hide()
-	{
-		if (mBasicLowerBannerAdView != null)
-		{
-			mBasicLowerBannerAdView.setVisibility(View.GONE);
+	public void hide() {
+		if (mBasicLowerBannerAdView != null) {
+			renovations3DActivity.runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					mBasicLowerBannerAdView.setEnabled(false);
+					mBasicLowerBannerAdView.setVisibility(View.GONE);
+				}
+			});
 			ViewGroup parent = (ViewGroup) mBasicLowerBannerAdView.getParent();
-			if (parent != null)
-			{
+			if (parent != null) {
 				parent.invalidate();
 			}
 		}
 	}
 
 	//used by the tutorial to not show ads until finished
-	public void show()
-	{
-		if (mBasicLowerBannerAdView != null && renovations3DActivity != null)
-		{
-			if (!shouldSuppressAds())
-			{
-				if (mBasicLowerBannerAdView != null)
-				{
-					mBasicLowerBannerAdView.setVisibility(View.VISIBLE);
+	public void show() {
+		if (mBasicLowerBannerAdView != null && renovations3DActivity != null) {
+			if (!shouldSuppressAds()) {
+				if (mBasicLowerBannerAdView != null) {
+					renovations3DActivity.runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							mBasicLowerBannerAdView.setEnabled(true);
+							mBasicLowerBannerAdView.setVisibility(View.VISIBLE);
+						}
+					});
 					ViewGroup parent = (ViewGroup) mBasicLowerBannerAdView.getParent();
-					if (parent != null)
-					{
+					if (parent != null) {
 						parent.invalidate();
 					}
 				}
@@ -233,9 +204,8 @@ public class AdMobManager
 
 
 
-	// interstital support
-	public enum InterstitialEventType
-	{
+	// interstitial support
+	public enum InterstitialEventType {
 		HOME_SAVE_AS, //1pt
 		HOME_SHARE, //1pt
 		NEW_HOME, //1pt
@@ -245,95 +215,11 @@ public class AdMobManager
 		IMPORT_BACKGROUND,//1pt
 	}
 
-	public void eventTriggered(InterstitialEventType type)
-	{
-		if (renovations3DActivity != null && !shouldSuppressAds())
-		{
-			SharedPreferences settings = renovations3DActivity.getSharedPreferences(Renovations3DActivity.PREFS_NAME, 0);
-			int prevPoints = settings.getInt(INTERSTITIAL_POINTS, 0);
-			int points = prevPoints + (type == InterstitialEventType.HOME_SAVE_AS ? 1 :
-					type == InterstitialEventType.HOME_SHARE ? 1 :
-							type == InterstitialEventType.NEW_HOME ? 1 :
-									type == InterstitialEventType.PHOTO_SAVE_OR_SHARE ? 2 :
-											type == InterstitialEventType.IMPORT_FURNITURE ? 1 :
-													type == InterstitialEventType.IMPORT_TEXTURE ? 1 :
-															type == InterstitialEventType.IMPORT_BACKGROUND ? 1 :
-																	0);
-
-			Renovations3DActivity.logFireBase(FirebaseAnalytics.Event.POST_SCORE, "INTERSTITIAL " + type.name(), "pt=" + points);
-
-			SharedPreferences.Editor editor = settings.edit();
-			editor.putInt(INTERSTITIAL_POINTS, points);
-			editor.apply();
-
-		}
+	public void eventTriggered(InterstitialEventType type) {
+		// Interstitials remove to reduce change of issues and I don't need the revenue.
 	}
 
-	public void interstitialDisplayPoint()
-	{
-		if (renovations3DActivity != null && !shouldSuppressAds())
-		{
-			renovations3DActivity.runOnUiThread(new Runnable()
-			{
-				public void run()
-				{
-					SharedPreferences settings = renovations3DActivity.getSharedPreferences(Renovations3DActivity.PREFS_NAME, 0);
-
-					int points = settings.getInt(INTERSTITIAL_POINTS, 0);
-
-					long lastShownTime = settings.getLong(INTERSTITIAL_LAST_SHOWN_TIME, 0);
-					// is now the time to show an interstitial?
-					if (points >= INTERSTITIAL_POINTS_THRESHOLD && System.currentTimeMillis() - lastShownTime > INTERSTITIAL_TIME_THRESHOLD)
-					{
-						// we reset either way as we still want the interstitial counts on firebase correct
-						// zero points and record exact time
-						SharedPreferences.Editor editor = settings.edit();
-						editor.putInt(INTERSTITIAL_POINTS, 0);
-						editor.putLong(INTERSTITIAL_LAST_SHOWN_TIME, System.currentTimeMillis());
-						editor.apply();
-
-						if (mFirebaseRemoteConfig.getBoolean("renovations3d_interstitial_enabled"))
-						{
-							if (mInterstitialAd != null)
-							{
-								Renovations3DActivity.logFireBaseContent("INTERSTITIAL SHOWN", null);
-								mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback(){
-									@Override
-									public void onAdDismissedFullScreenContent() {
-										// Called when fullscreen content is dismissed.
-										//Log.d("TAG", "The ad was dismissed.");
-									}
-
-									@Override
-									public void onAdFailedToShowFullScreenContent(AdError adError) {
-										// Called when fullscreen content failed to show.
-										//Log.d("TAG", "The ad failed to show.");
-									}
-
-									@Override
-									public void onAdShowedFullScreenContent() {
-										// Called when fullscreen content is shown.
-										// Make sure to set your reference to null so you don't
-										// show it a second time.
-										mInterstitialAd = null;
-										//Log.d("TAG", "The ad was shown.");
-									}
-								});
-								mInterstitialAd.show(renovations3DActivity);
-
-								//if more than one per day and session then loadAd needs to be called here
-							}
-							else
-							{
-								Renovations3DActivity.logFireBase(FirebaseAnalytics.Event.POST_SCORE, "INTERSTITIAL NOT LOADED", null);
-							}
-						}
-						else
-						{
-							Renovations3DActivity.logFireBaseContent("INTERSTITIAL WOULD HAVE BEEN SHOWN", null);
-						}
-					}
-				}});
-		}
+	public void interstitialDisplayPoint() {
+		// Interstitials remove to reduce change of issues and I don't need the revenue.
 	}
 }
